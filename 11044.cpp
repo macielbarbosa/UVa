c1be6dcf452c2cdcf0b8fc10b2f1bb752badae40
Maciel Barbosa da Silva <macielbarbosa@LCC.UFRN.BR>
2017-07-28 18:28:54 -0300

    Searching for Nessy

---
+++ 11044.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int t, n, m;
+    cin >> t;
+    while(t-->0){
+        cin >> n >> m;
+        cout << (n/3)*(m/3) << endl;
+    }
+}
