7c7350709891eaa14dc767c422bf647b5dcc51fe
macielbarbosa <macielbarbosa@outlook.com>
2017-08-03 16:51:52 -0300

    Zeros and Ones
    incompletro

---
+++ 10324.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void ordenar(int &i, int &j){
+    int aux;
+    if(j>i){
+        aux = i;
+        i = j;
+        j = aux;
+    }
+}
+
+int main(void){
+    char str[1000000], carac;
+    int n,i,j,k,caso=1;
+    bool igual;
+
+    while(scanf("%s", &str)!= EOF){
+        printf("Caso %d:\n", caso++);
+        cin >> n;
+        while(n--){
+            igual = true;
+            cin >> i >> j;
+            ordenar(i,j);
+            carac = str[i];
+            for(k=i; k<=j; k++){
+                if(str[k]!= carac){
+                    igual = false;
+                    break;
+                }
+            }
+            if(igual)
+                cout << "Yes\n";
+            else
+                cout << "No\n";
+        }
+    }
+}
